d5113eacc5c3641a0219b19768e1f5eee3984ee3
johan_puentes_dev <puentespulidojohansebastian@gmail.com>
2021-08-18 03:18:57 +0000

    first commit

--- myvar2.sh
+++ myvar2.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
-export MYVAR
-echo "MYVAR is: $MYVAR"
-echo "MYVAR is: $MYVAR"
+#! /bin/bash
+
+# echo "hello my name is $1, im from of $2 in the city $3"
+
+# this is a summon a number of the user(you)
+echo $(whoami)
---
+++ reboot.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+shutdown -r now
+# hello, run in your terminal sudo ./reboot.sh and restart the system.
